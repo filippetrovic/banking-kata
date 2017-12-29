5400f0401dfe512e9612f26f2c77e0f5d5d6ed80
Filip Petrovic <fpetrovic92@gmail.com>
2017-12-29 18:28:31 +0100

    Kata skeleton

---
+++ src/main/java/kata/banking/Account.java
@@ -0,0 +1,16 @@
+package kata.banking;
+
+public class Account {
+
+    public void deposit(int amount) {
+
+    }
+
+    public void withdraw(int amount) {
+
+    }
+
+    public String printStatement() {
+        return null;
+    }
+}
