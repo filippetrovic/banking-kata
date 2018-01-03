1be10b9cc2af963d3eb30fe31e7baf9e47ac2011
Filip Petrovic <fpetrovic92@gmail.com>
2018-01-03 19:26:39 +0100

    Verbosity improved

--- src/main/java/kata/banking/domain/BalanceLog.java
+++ src/main/java/kata/banking/domain/BalanceLog.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class BalanceLog {
 
     private List<Transaction> transactions = new LinkedList<>();
-    private int totalBalance;
+    private int totalBalance = 0;
 
     public BalanceLog() {
     }
